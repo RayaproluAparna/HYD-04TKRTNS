328b010f81b03d0c803f5c5383f48bc28c742dd2
RayaproluAparna <142445694+RayaproluAparna@users.noreply.github.com>
2023-11-21 16:18:26 +0530

    Admin.java

--- Admin.java
+++ Admin.java
@@ -1,45 +1,30 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2636
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh15020\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\partightenfactor0
-
-\f0\fs24 \cf0 package com.tns.controller;\
-/* Created by APARNA RAYAPROLU\
-      *21-11-23\
-      */\
-public class Admin \{\
-	long id;\
-	String name;\
-	String password;\
-\
-	public Admin() \{\
-	\
-	\}\
-\
-	public long getId() \{\
-		return id;\
-	\}\
-\
-	public void setId(long id) \{\
-		this.id = id;\
-	\}\
-\
-	public String getName() \{\
-		return name;\
-	\}\
-\
-	public void setName(String name) \{\
-		this.name = name;\
-	\}\
-\
-	public String getPassword() \{\
-		return password;\
-	\}\
-\
-	public void setPassword(String password) \{\
-		this.password = password;\
-	\}\
-\
-\}}
+ package com.tns.controller;
+/* Created by APARNA RAYAPROLU
+      *21-11-23
+      */
+public class Admin {
+	long id;
+	String name;
+	String password;
+	public Admin() {
+	
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
